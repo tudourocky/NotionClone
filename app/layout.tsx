c1d8cb5f0935e3dc604c18b460c7c9b96bc21bd6
tudourocky <67175442+tudourocky@users.noreply.github.com>
2023-11-07 23:11:38 -0500

    Uploaded source code onto Github

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
+import { Toaster } from "sonner"
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ConvexClientProvider } from '@/components/providers/convex-provider';
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,14 +35,18 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-        <ThemeProvider attribute='class'
-        defaultTheme='system'
-        enableSystem
-        disableTransitionOnChange
-        storageKey='notion-theme-2'
-        >
-          {children}
-        </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider attribute='class'
+              defaultTheme='system'
+              enableSystem
+              disableTransitionOnChange
+              storageKey='notion-theme-2'
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
